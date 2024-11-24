d3a06c329d5c4a3198b5ac8463b46188b9b07973
MKLV-19 <2016181214@qq.com>
2024-11-25 02:52:01 +0800

    '更新了素材'

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
 import pygame
-import time
 import random
 
 # 初始化pygame
@@ -28,8 +27,18 @@ dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('贪吃曹')
 
 # 加载蛇头图片
-snake_head_image = pygame.image.load(r'D:\python_EX\Snake\head2.jpg')
+snake_head_image = pygame.image.load(r'D:\python_EX\Snake\new_head.jpg')
 snake_head_image = pygame.transform.scale(snake_head_image, (snake_block, snake_block))
+
+# 加载身体图片 
+snake_body_image = pygame.image.load(r'D:\python_EX\Snake\new_body.jpg')
+snake_body_image = pygame.transform.scale(snake_body_image, (snake_block, snake_block))
+
+# 加载转身图片
+snake_turnleft_image = pygame.image.load(r'D:\python_EX\Snake\new_turning_left.jpg')
+snake_turnleft_image = pygame.transform.scale(snake_turnleft_image, (snake_block, snake_block))
+snake_turnright_image = pygame.image.load(r'D:\python_EX\Snake\new_turning_right.jpg')
+snake_turnright_image = pygame.transform.scale(snake_turnright_image, (snake_block, snake_block))
 
 # 加载食物图片
 food_image = pygame.image.load(r"D:\python_EX\Snake\head.jpg")
@@ -42,16 +51,39 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont(None, 50)
 score_font = pygame.font.SysFont(None, 35)
 
+def is_clockwise(head_direct, tail_direct):
+    """
+    判断是否为顺时针旋转
+    """
+    if (head_direct-tail_direct == 90) or (head_direct == 0 and tail_direct == 270):
+        return True
+    else: 
+        return False
 
-def our_snake(snake_block, snake_list):
+def our_snake(snake_block, snake_list, snake_rotate_angle_list):
     for i, x in enumerate(snake_list):
         if i == len(snake_list)-1:
             # 绘制蛇头
-            rotated_head = pygame.transform.rotate(snake_head_image, snake_direction_angle)
+            rotated_head = pygame.transform.rotate(snake_head_image, snake_rotate_angle_list[i])
             dis.blit(rotated_head, [x[0], x[1]])
         else:
-            # 绘制蛇身
-            pygame.draw.rect(dis, orange, [x[0], x[1], snake_block, snake_block])
+            #首先判断是否为转弯点，即判断下一个旋转角度是否与当前角度不同
+            if snake_rotate_angle_list[i] != snake_rotate_angle_list[i+1]:
+                if is_clockwise(snake_rotate_angle_list[i+1], snake_rotate_angle_list[i]):
+                    # 计算旋转角度
+                    body_rotate_angle = snake_rotate_angle_list[i+1] - 90
+                    rotated_turn_body = pygame.transform.rotate(snake_turnleft_image, body_rotate_angle)
+                else:
+                    body_rotate_angle = snake_rotate_angle_list[i+1] + 90
+                    rotated_turn_body = pygame.transform.rotate(snake_turnright_image, body_rotate_angle)
+                # 绘制
+                dis.blit(rotated_turn_body, [x[0], x[1]])
+            else :
+                # 绘制蛇身
+                rotated_body = pygame.transform.rotate(snake_body_image, snake_rotate_angle_list[i])
+                dis.blit(rotated_body, [x[0], x[1]])
+                # pygame.draw.rect(dis, orange, [x[0], x[1], snake_block, snake_block])
+                
 
 
 def message(msg, color):
@@ -68,12 +100,15 @@ def gameLoop():
     x1 = dis_width / 2
     y1 = dis_height / 2
 
-    x1_change = 0
-    y1_change = -snake_block  # 默认向上移动
-    snake_direction_angle = 0  # 蛇头初始方向角度为0度（向上）
+    x1_change = snake_block
+    y1_change = 0  # 默认向右侧移动
+    snake_direction_angle = 270  # 蛇头初始方向角度为0度（向上）
 
     snake_List = []
     Length_of_snake = 1
+    
+    # 再开一个列表记录每块身体对应的旋转角度
+    snake_rotate_angle_list = []
 
     foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
     foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
@@ -125,16 +160,18 @@ def gameLoop():
         snake_Head.append(x1)
         snake_Head.append(y1)
         snake_List.append(snake_Head)
+        snake_rotate_angle_list.append(snake_direction_angle) #保存当前身体朝向
         # 每次循环向change的方向添加一格像素，同时删除最开始的一格像素，从而达到前进的效果
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
+            del snake_rotate_angle_list[0]
 
         # 自我碰撞判定
         for x in snake_List[:-1]:
             if x == snake_Head:
                 game_close = True
 
-        our_snake(snake_block, snake_List)
+        our_snake(snake_block, snake_List, snake_rotate_angle_list)
 
         pygame.display.update()
 
